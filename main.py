cef33d4b5af71f49b03ea246a0d1d651e77af3e1
Marko Vukušić <marko@codesplitters.com>
2020-02-21 22:01:28 +0100

    Implement data storage to file

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
+import pickle
 
 URL = 'https://www.canyon.com/en-hr/outlet/road-bikes/?cgid=outlet-road&prefn1=pc_outlet&prefn2=pc_rahmengroesse&prefv1=true&prefv2=L&srule=sort_price_ascending'
+storedBikesFileName = 'bikes.data'
 
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+def readFromFile(name):
+    with open(name, 'rb') as filehandle:
+        return pickle.load(filehandle)
 
-bikes = soup.find_all('div', class_='productTile__contentWrapper')
+def writeToFile(name, data):
+    with open(storedBikesFileName, 'wb') as filehandle:
+        pickle.dump(data, filehandle)
 
-content = []
+def scrapeContent():
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-# print("%-*s %-*s %s" % (40, "Model name", 20, "Regular price", "Sale price"))
-for bike in bikes:
-    name = bike.find('span', class_='productTile__productName').text.strip()
-    regularPrice = bike.find('span', class_='productTile__productPriceOriginal').text.strip()
-    salePrice = bike.find('span', class_='productTile__productPriceSale').text.strip()
+    bikes = soup.find_all('div', class_='productTile__contentWrapper')
+    # print("%-*s %-*s %s" % (40, "Model name", 20, "Regular price", "Sale price"))
+    filteredContent = []
+    for bike in bikes:
+        name = bike.find('span', class_='productTile__productName').text.strip()
+        regularPrice = bike.find('span', class_='productTile__productPriceOriginal').text.strip()
+        salePrice = bike.find('span', class_='productTile__productPriceSale').text.strip()
 
-    if None in (name, regularPrice, salePrice):
-        continue
-    
-    content.append([name, regularPrice, salePrice])
-    # print("%-*s %-*s %s" % (40, name, 20, regularPrice, salePrice))
+        if None in (name, regularPrice, salePrice):
+            continue
+        
+        filteredContent.append([name, regularPrice, salePrice])
+        # print("%-*s %-*s %s" % (40, name, 20, regularPrice, salePrice))
+    return filteredContent
 
-print(content)
+content = scrapeContent()
+
+storedBikes = readFromFile(storedBikesFileName)
+print(storedBikes)
+
+writeToFile(storedBikesFileName, content)
+
+
