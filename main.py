4bbac16c8ea0da5530a9aee58cf5bf890a972888
Marko Vukušić <marko@codesplitters.com>
2020-02-29 23:59:05 +0100

    Implement argument filtering

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from services.scrape_service import scrapeContent
 from services.file_service import FileService
 from services.email_service import sendMail
 from services.argument_parser import ArgumentParser
+from helper.url_builder import buildUrl
 
 import numpy as np
 import os
@@ -10,9 +11,11 @@ appPath = os.path.dirname(os.path.abspath(__file__))
 fileService = FileService(appPath)
 
 arguments = ArgumentParser()
-currentBikeList = scrapeContent(arguments.url)
-storedBikeList = fileService.readFromFile(arguments.fileName)
+url = buildUrl(arguments)
 
+currentBikeList = scrapeContent(url)
+storedBikeList = fileService.readFromFile(arguments.fileName)
+print(currentBikeList)
 newBikesExists = not np.array_equal(currentBikeList, storedBikeList)
 if newBikesExists:
     newBikes = [item for item in currentBikeList if item not in storedBikeList]
--- services/argument_parser.py
+++ services/argument_parser.py
@@ -6,12 +6,17 @@ class ArgumentParser:
         self.__readArguments()
     
     def __configureArguments(self):
+        typeChoices = ["road", "mountain", "urban", "fitness", "pro"]
+        sizeChoices = ["3XS", "2XS", "XS", "S", "M", "L", "XL", "2XL", "3XL"]
+        genderChoices = ["Unisex", "WMNS"]
+        
         parser = argparse.ArgumentParser()
-        parser.add_argument("-e", "--email",        help="Set email",                   required=True)
-        parser.add_argument("-l", "--localization", help="Set website localization",    required=True)
-        parser.add_argument("-t", "--type",         help="Set bike type",               required=True)
-        parser.add_argument("-s", "--size",         help="Set bike size",               required=True)
-        parser.add_argument("-m", "--model",        help="Set bike model",              required=True)
+        parser.add_argument("-e", "--email",        help="Set email",                           required=True, type=str)
+        parser.add_argument("-l", "--localization", help="Set website localization",            required=True, type=str)
+        parser.add_argument("-t", "--type",         help="Set bike type",                       required=True, type=str, choices=typeChoices)
+        parser.add_argument("-s", "--size",         help="Set bike size",                       required=True, type=str, choices=sizeChoices)
+        parser.add_argument("-m", "--model",        help="Set bike models(comma separated)",    required=True, type=str)
+        parser.add_argument("-g", "--gender",       help="Set gender (Unisex/WMNS)",            required=True, type=str, choices=genderChoices)
 
         self.__args = parser.parse_args()
     
@@ -21,11 +26,13 @@ class ArgumentParser:
         if not re.match(emailRegex, self.email):
             print("Error: Email address invalid")
             exit(1)
-            
-            self.fileName = self.email.split("@")[0] + ".data"
 
-        self.url = self.__args.url
+        self.fileName = self.email.split("@")[0] + ".data"
+
         self.localization = self.__args.localization
         self.type = self.__args.type
         self.size = self.__args.size
-        self.model = self.__args.model
+        self.model = self.__args.model.replace(",", "%7C")
+        self.gender = self.__args.gender
+
+        
